54cf7dd76337a3572233902f2290500b05ffad06
Lord Stark <strangexstark@gmail.com>
2025-12-30 01:22:42 +0530

    Configure Django admin as CMS

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Class, Subject, Chapter, Resource
 
-# Register your models here.
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'class_level')
+    list_filter = ('class_level',)
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subject')
+    list_filter = ('subject',)
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'resource_type', 'chapter')
+    list_filter = ('resource_type', 'chapter')
+    search_fields = ('title',)
